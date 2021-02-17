507554c019575932499c778b84a943cd3617f994
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2021-02-17 20:54:26 +0530

    Global style added with Styledcomponent and Theme set using themeprovider

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,60 @@
 import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+//Styled Component
+
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+import { normalize } from "styled-normalize"
+
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  * {
+    text-decoration: none
+    //cursor:none;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-size: 16px;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxegen, Ubantu, 'Open Sans', sans-serif;
+    background: ${props => props.theme.background};
+    overscroll-behavior: none;
+    overflow-x: hidden;
+  }
+
+
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
+      query SiteTitleQuery {
+          site {
+              siteMetadata {
+                  title
+              }
+          }
       }
-    }
   `)
 
-  return <main>{children}</main>
+  const darkTheme = {
+    background: "#000",
+    text: "#fff",
+  }
+  const lightTheme = {
+    text: "#000",
+    background: "#fff",
+  }
+
+  return <ThemeProvider theme={darkTheme}>
+
+    <GlobalStyle />
+    <main>{children}</main>
+  </ThemeProvider>
 }
 
 Layout.propTypes = {
